7b257fb2829d8efb19febade03fb5d98c895c87b
devMtn <97010280+devMtn30@users.noreply.github.com>
2022-02-01 14:13:13 +0000

    day 19

--- 백준/1110.py
+++ 백준/1110.py
@@ -18,19 +18,18 @@ cyclenum2 = 0
 cyclenum = N
 cnt=0
 while True:
+  cnt+=1
   if int(cyclenum) < 10 :
     fnum = 0
-    bnum = N
+    bnum = int(str(cyclenum)[-1])
   else :
     fnum = int(str(cyclenum)[0])
-    bnum = int(str(cyclenum)[1])
+    bnum = int(str(cyclenum)[-1])
   
   cyclenum = int(str(bnum)+str(fnum+bnum)[-1:])
   if cyclenum == cyclenum2:
     break;
   cyclenum2 = cyclenum
-  print(cyclenum,cyclenum2)
-  cnt+=1
   if int(cyclenum) == N:
     break;
   
