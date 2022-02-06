355657bd5a3d69ec64d3711e0dda0086279ec31d
devMtn <97010280+devMtn30@users.noreply.github.com>
2022-02-06 14:35:06 +0000

    day 23

--- 1316.py
+++ 1316.py
@@ -22,10 +22,6 @@ for i in range(n):
 #그룹단어인지 비교
 for i in S:
   alphabet = {'a':0,'b':0,'c':0,'d':0,'e':0,'f':0,'g':0,'h':0,'i':0,'j':0,'k':0,'l':0,'m':0,'n':0,'o':0,'p':0,'q':0,'r':0,'s':0,'t':0,'u':0,'v':0,'w':0,'x':0,'y':0,'z':0}
-  temp = i[0]
+
   for alpha in i:
-    if alphabet[alpha] !=0:
-      break;
-    if temp !=alpha:
-      alphabet[alpha] +=1
-      
+    print(alpha)
---
+++ 백준/1157.py
@@ -0,0 +1,29 @@
+# 문제
+# 알파벳 대소문자로 된 단어가 주어지면, 이 단어에서 가장 많이 사용된 알파벳이 무엇인지 알아내는 프로그램을 작성하시오. 단, 대문자와 소문자를 구분하지 않는다.
+
+# 입력
+# 첫째 줄에 알파벳 대소문자로 이루어진 단어가 주어진다. 주어지는 단어의 길이는 1,000,000을 넘지 않는다.
+
+# 출력
+# 첫째 줄에 이 단어에서 가장 많이 사용된 알파벳을 대문자로 출력한다. 단, 가장 많이 사용된 알파벳이 여러 개 존재하는 경우에는 ?를 출력한다.
+
+words = list(input())
+
+alphabet = {'a':0,'b':0,'c':0,'d':0,'e':0,'f':0,'g':0,'h':0,'i':0,'j':0,'k':0,'l':0,'m':0,'n':0,'o':0,'p':0,'q':0,'r':0,'s':0,'t':0,'u':0,'v':0,'w':0,'x':0,'y':0,'z':0}
+
+
+for word in words:
+  for alpha in alphabet.keys():
+    if word.upper() == alpha.upper():
+      alphabet[alpha] +=1
+
+maxalpha = [k for k,v in alphabet.items() if max(alphabet.values()) == v]
+if len(maxalpha) > 1:
+  print("?")
+else :
+  print(maxalpha[0].upper())
+
+
+
+
+      
