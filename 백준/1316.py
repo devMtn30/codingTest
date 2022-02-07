addf7cf33ce020887a5473423c1ac76167d5607a
devMtn <97010280+devMtn30@users.noreply.github.com>
2022-02-07 14:41:52 +0000

    day 24

--- 백준/1316.py
+++ 백준/1316.py
@@ -14,14 +14,24 @@
 
 
 n = int(input())
-
+print(int(n))
 S = []
 for i in range(n): 
   S.append(list(input()))
 
 #그룹단어인지 비교
-for i in S:
-  alphabet = {'a':0,'b':0,'c':0,'d':0,'e':0,'f':0,'g':0,'h':0,'i':0,'j':0,'k':0,'l':0,'m':0,'n':0,'o':0,'p':0,'q':0,'r':0,'s':0,'t':0,'u':0,'v':0,'w':0,'x':0,'y':0,'z':0}
-
-  for alpha in i:
-    print(alpha)
+for word in S:
+  arr = []
+  #문자를 하나씩 i에 넣기
+  for i in word:
+    temp = i
+    #글자가 이미 arr에 있으면 종료
+    if temp in arr and pre != temp:
+      n-=1
+      break
+    arr.append(temp)
+    pre = temp
+print(n)
+    
+    
+      
