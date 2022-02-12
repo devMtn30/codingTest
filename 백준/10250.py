01fee1080db786101d2dbbccd497c1dbb4f22311
devMtn <97010280+devMtn30@users.noreply.github.com>
2022-02-12 14:34:03 +0000

    day 29

--- 백준/10250.py
+++ 백준/10250.py
@@ -7,15 +7,21 @@ T = int(input())
 result = []
 for i in range(T):
   H,W,N = map(int,input().split())
+
   rm = math.ceil(N//H)+1
   floor = N%H
-
-  #if floor == 0:
-    
+  
+  if H >= N:
+    floor = N
+    rm = 1
+  if floor == 0:
+    floor = H
+    rm = math.ceil(N//H)
   if rm >=10:
     result.append(str(floor)+str(rm))
-  else:
+  else:   
     result.append(str(floor)+'0'+str(rm))
 
 for res in result:
   print(res)
+  
