092c1b16d17388f1957ca11aa0b9b222bc45dd6c
[devmtn30] <[devmtn30@gmail.com]>
2022-03-05 17:06:28 +0900

    day 50

--- 9020.py
+++ 9020.py
@@ -16,25 +16,27 @@ for i in range(2, 10001):  # sqrt +1까지만 루프 돌면 됨
 primeNum1 = 2
 primeNum2 = 2
 arr = []
-
 N = int(sys.stdin.readline().rstrip())
 for i in range(N):
     arr.append(int(sys.stdin.readline().rstrip()))
+
 # 골드바흐 파티션 구하기
 for x in arr:
     flag = False
-    for j in numArr:
+    numArr2 = []
+    for j in range(int(x/2)):#첫번째 소수를 구하고, 두번째 소수는 첫번째 소수의 큰것부터 작은것까지 넣어서 비교해본다.
         if flag:
             break
-        for k in numArr:
+        numArr2.append(numArr[j])
+        for k in reversed(numArr2):
             if k+k == x:
                 primeNum1 = k
                 primeNum2 = k
                 flag = True
                 break
-            if j + k == x:
-                primeNum1 = j
-                primeNum2 = k
+            if numArr[j] + k == x:
+                primeNum1 = k
+                primeNum2 = numArr[j]
                 flag = True
                 break
     print(primeNum1, primeNum2)
