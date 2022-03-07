e2962b4604f79ba41d396cc0d6f73e2af4ae9452
[devmtn30] <[devmtn30@gmail.com]>
2022-03-07 21:42:07 +0900

    day 52

--- 9020.py
+++ 9020.py
@@ -1,42 +1,17 @@
 import sys
-import math
 
-numArr = []
+
 primeNum = [True] * 10001
+primeNum[1] = False
 # 소수 구하기
-for i in range(2, 10001):  # sqrt +1까지만 루프 돌면 됨
-    if primeNum[i]:
-        numArr.append(i)
-        j = 2
-        while i * j <= 10000:  # 에라토스테네스의 체 구현
-            primeNum[i * j] = False
-            j += 1
-
+for i in range(2, 98):
+    for j in range(i * 2, 10001, i):
+        primeNum[j] = False
 # 입력 받기
-primeNum1 = 2
-primeNum2 = 2
-arr = []
 N = int(sys.stdin.readline().rstrip())
 for i in range(N):
-    arr.append(int(sys.stdin.readline().rstrip()))
-
-# 골드바흐 파티션 구하기
-for x in arr:
-    flag = False
-    numArr2 = []
-    for j in range(int(x/2)):#첫번째 소수를 구하고, 두번째 소수는 첫번째 소수의 큰것부터 작은것까지 넣어서 비교해본다.
-        if flag:
+    a = int(sys.stdin.readline().rstrip())
+    for j in range(a//2, 0, -1):
+        if primeNum[j] and primeNum[a - j]:
+            print(j, a - j)
             break
-        numArr2.append(numArr[j])
-        for k in reversed(numArr2):
-            if k+k == x:
-                primeNum1 = k
-                primeNum2 = k
-                flag = True
-                break
-            if numArr[j] + k == x:
-                primeNum1 = k
-                primeNum2 = numArr[j]
-                flag = True
-                break
-    print(primeNum1, primeNum2)
