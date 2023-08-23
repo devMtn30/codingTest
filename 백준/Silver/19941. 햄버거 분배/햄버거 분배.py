ec29a137dc2ca3cddf07ebc4395db34d4cffcd0e
KIM SAN <devmtn30@gmail.com>
2023-08-23 21:48:10 +0900

    [Silver III] Title: 햄버거 분배, Time: 88 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/19941. 햄버거 분배/햄버거 분배.py
+++ 백준/Silver/19941. 햄버거 분배/햄버거 분배.py
@@ -1,21 +1,16 @@
-# https://www.acmicpc.net/problem/19941
 
 N, K = map(int, input().split())
 arr = list(input())
-rangeList = []
-for i in range(-K,K+1): #먹을 수 있는 범위 (음수부터)
-    rangeList.append(i)
+rangeList = [i for i in range(-K, K + 1)]  # 먹을 수 있는 범위 (음수부터)
 
+answer = 0
 for i in range(N):
     if arr[i] == "P":
         for j in rangeList:
-            now = i+j
+            now = i + j
             if 0 <= now < N and arr[now] == "H":
-                arr[now] = "E" #Eat
+                arr[now] = "E"  # Eat 처리
+                answer += 1
                 break
 
-answer = 0
-for i in arr:
-    if i == "E":
-        answer+=1
 print(answer)
