39fb5e5b516ba9c4a6f2a9a15c1a9feb634ab654
KIM SAN <devmtn30@gmail.com>
2023-08-24 20:56:57 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -0,0 +1,11 @@
+expression = input().strip()
+
+operands = expression.split('-')
+
+# 첫 번째 연산자 이전까지의 숫자들은 더하기
+total = sum(map(int, operands[0].split('+')))
+
+for operand in operands[1:]:
+    total -= sum(map(int, operand.split('+')))
+
+print(total)
