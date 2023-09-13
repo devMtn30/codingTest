a33a43bf946e3fb2f9e88ace886e293f36c35740
KIM SAN <devmtn30@gmail.com>
2023-09-13 20:11:55 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 64 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,24 +1,29 @@
-n = int(input())
-number = list(map(int, input().split()))
-add, sub, mul, div = map(int, input().split());
-max_result = - int(1e9)
-min_result = int(1e9)
+import sys;
+input = sys.stdin.readline
 
-def dfs(add, sub, mul, div, sum, idx):
-    global max_result, min_result
-    if idx == n:
-        max_result = max(max_result, sum)
-        min_result = min(min_result, sum)
+n = int(input())
+numbers = list(map(int, input().split()))
+add, sub, mul, div = map(int, input().split())# + - * /
+
+maxNum = - int(1e9)
+minNum = int(1e9)
+
+
+def dfs(add, sub, mul, div, total, i):
+    global maxNum, minNum
+    if i == n:
+        maxNum = max(maxNum, total)
+        minNum = min(minNum, total)
         return
     if add:
-        dfs(add-1, sub, mul, div, sum + number[idx], idx + 1)
+      dfs(add-1, sub, mul, div, total + numbers[i], i+1)
     if sub:
-        dfs(add, sub-1, mul, div, sum - number[idx], idx + 1)
+      dfs(add, sub-1, mul, div, total - numbers[i], i+1)
     if mul:
-        dfs(add, sub, mul-1, div, sum * number[idx], idx + 1)
+      dfs(add, sub, mul-1, div, total * numbers[i], i+1)
     if div:
-        dfs(add, sub, mul, div-1, int(sum / number[idx]), idx + 1)
-        
-dfs(add, sub, mul, div, number[0], 1)
-print(max_result)
-print(min_result)
+      dfs(add, sub, mul, div-1, int(total / numbers[i]), i+1)
+
+dfs(add, sub, mul, div, numbers[0], 1)
+print(maxNum)
+print(minNum)
